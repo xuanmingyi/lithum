cf76213808f3f103be78a13840a104e3cb91db4d
sin <xuanmingyi@gmail.com>
2021-01-03 22:49:02 +0800

    init project

---
+++ engine/go.mod
@@ -0,0 +1,8 @@
+module engine
+
+go 1.14
+
+require (
+	github.com/yuin/gopher-lua v0.0.0-20200816102855-ee81675732da // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ engine/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	lua "github.com/yuin/gopher-lua"
+	yaml "gopkg.in/yaml.v2"
+	"io/ioutil"
+	"sync"
+)
+
+type Task struct {
+	Name string `yaml:"name"`
+	Path string `yaml:"path"`
+}
+
+
+
+func RunPipeline(task Task, group *sync.WaitGroup) {
+	defer group.Done()
+
+	path := fmt.Sprintf("%s/main.lua", task.Path)
+	fmt.Println(path)
+
+	L := lua.NewState()
+	defer L.Close()
+
+	if err := L.DoString(`print("hello world")`); err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	var tasks *[]Task
+	wg := sync.WaitGroup{}
+	tasks = new([]Task)
+
+	content, err := ioutil.ReadFile("pipelines.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.Unmarshal(content, tasks)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, v := range *tasks {
+		wg.Add(1)
+		go RunPipeline(v, &wg)
+	}
+	wg.Wait()
+
+
+}
