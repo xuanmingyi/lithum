8ef95d9a57854c49e2e2bf44be8a994e74489b39
sin <xuanmingyi@gmail.com>
2021-02-24 21:21:15 +0800

    add unknown error

--- pyengine/pipelines/lvv2_image_downloader.py
+++ pyengine/pipelines/lvv2_image_downloader.py
@@ -21,11 +21,14 @@ class LVV2ImageDownloaderPipeline(BasePipeline):
     def task(self):
         images = self.session.query(LVV2Image).filter(LVV2Image.status=="new").all()
         for _image in images:
-            status_code = download_image(_image.url, os.path.join(BASE_OUTPUT, "lvv2", str(_image.date), self.get_thread_by_id(_image.thread_id).title))
-            if status_code == 200:
-                _image.status = "download"
-            if status_code == 404:
-                _image.status = "notfound"
+            try:
+                status_code = download_image(_image.url, os.path.join(BASE_OUTPUT, "lvv2", str(_image.date), self.get_thread_by_id(_image.thread_id).title))
+                if status_code == 200:
+                    _image.status = "download"
+                if status_code == 404:
+                    _image.status = "notfound"
+            except Exception as e:
+                _image.status = str(e)
             self.session.commit()
             time.sleep(1)
 
